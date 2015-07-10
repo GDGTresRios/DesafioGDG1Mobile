4fb7bdf5244a80f5a48979d494c8d656f20bbd2c
Tássio Auad <auadtassio@gmail.com>
2015-07-10 02:03:35 -0300

    Activity Principal com viewpager exibindo os adapter de Próximos Eventos, Categoria de Eventos e Categoria de Colaboradores

---
+++ app/src/main/java/br/com/gdgtresrios/centrosultnegocios/controller/activity/HomeActivity.java
@@ -0,0 +1,62 @@
+package br.com.gdgtresrios.centrosultnegocios.controller.activity;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+
+import br.com.gdgtresrios.centrosulnegocios.R;
+import br.com.gdgtresrios.centrosultnegocios.controller.fragment.CategoriaEventoFragment;
+import br.com.gdgtresrios.centrosultnegocios.controller.fragment.ProximosEventosFragment;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        viewPager.setAdapter(new PagerAdapter());
+    }
+
+    private class PagerAdapter extends FragmentPagerAdapter {
+
+        public PagerAdapter() {
+            super(getSupportFragmentManager());
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position) {
+                default:
+                case 0:
+                    return new ProximosEventosFragment();
+                case 1:
+                    return new CategoriaEventoFragment();
+                case 2:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            switch (position) {
+                default:
+                case 0:
+                    return getString(R.string.fragment_proximoseventos_title);
+                case 1:
+                    return getString(R.string.fragment_categoriaeventos_title);
+                case 2:
+                    return getString(R.string.fragment_categoriacolaboradores_title);
+            }
+        }
+    }
+
+
+}
