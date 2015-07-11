ca1886d72e796d493659663f1a65571095879b8a
Tássio Auad <auadtassio@gmail.com>
2015-07-11 02:09:09 -0300

    Alguns inserts no banco para teste

--- app/src/main/java/br/com/gdgtresrios/centrosulnegocios/model/dao/DatabaseConnection.java
+++ app/src/main/java/br/com/gdgtresrios/centrosulnegocios/model/dao/DatabaseConnection.java
@@ -8,6 +8,9 @@ import android.util.Log;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.com.gdgtresrios.centrosulnegocios.model.CategoriaColaborador;
+import br.com.gdgtresrios.centrosulnegocios.model.CategoriaEvento;
+
 public class DatabaseConnection extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "centrosulnegocios";
@@ -28,6 +31,16 @@ public class DatabaseConnection extends SQLiteOpenHelper {
         db.execSQL(ColaboradorDao.CREATE_TABLE);
         db.execSQL(EventoDao.CREATE_TABLE);
         db.execSQL(ConfiguracaoDao.CREATE_TABLE);
+
+        db.execSQL("INSERT INTO " + CategoriaEventoDao.TABLE_NAME + "(" + CategoriaEventoDao.COLUMN_NOME + ") VALUES('Palestras')");
+        db.execSQL("INSERT INTO " + CategoriaEventoDao.TABLE_NAME + "(" + CategoriaEventoDao.COLUMN_NOME + ") VALUES('Shows')");
+        db.execSQL("INSERT INTO " + CategoriaEventoDao.TABLE_NAME + "(" + CategoriaEventoDao.COLUMN_NOME + ") VALUES('Workshops')");
+        db.execSQL("INSERT INTO " + CategoriaEventoDao.TABLE_NAME + "(" + CategoriaEventoDao.COLUMN_NOME + ") VALUES('Estandes')");
+
+        db.execSQL("INSERT INTO " + CategoriaColaboradorDao.TABLE_NAME + "(" + CategoriaColaboradorDao.COLUMN_NOME + ") VALUES('Informática')");
+        db.execSQL("INSERT INTO " + CategoriaColaboradorDao.TABLE_NAME + "(" + CategoriaColaboradorDao.COLUMN_NOME + ") VALUES('Bandas')");
+        db.execSQL("INSERT INTO " + CategoriaColaboradorDao.TABLE_NAME + "(" + CategoriaColaboradorDao.COLUMN_NOME + ") VALUES('Comércio')");
+        db.execSQL("INSERT INTO " + CategoriaColaboradorDao.TABLE_NAME + "(" + CategoriaColaboradorDao.COLUMN_NOME + ") VALUES('Turismo')");
     }
 
     @Override
