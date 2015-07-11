5d7304b41def60250801df283cc1fd771300dd7e
Tássio Auad <auadtassio@gmail.com>
2015-07-11 18:42:58 -0300

    HomeActivity se tornou EventoActivity e continua como Activity principal. NavigationDrawer adicionado.

--- app/src/main/java/br/com/gdgtresrios/centrosultnegocios/controller/activity/EventoActivity.java
+++ app/src/main/java/br/com/gdgtresrios/centrosultnegocios/controller/activity/EventoActivity.java
@@ -1,31 +1,68 @@
 package br.com.gdgtresrios.centrosultnegocios.controller.activity;
 
-import android.database.sqlite.SQLiteDatabase;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import br.com.gdgtresrios.centrosulnegocios.R;
-import br.com.gdgtresrios.centrosulnegocios.model.dao.DatabaseConnection;
-import br.com.gdgtresrios.centrosultnegocios.controller.fragment.CategoriaColaboradorFragment;
 import br.com.gdgtresrios.centrosultnegocios.controller.fragment.CategoriaEventoFragment;
 import br.com.gdgtresrios.centrosultnegocios.controller.fragment.ProximosEventosFragment;
 
-public class HomeActivity extends AppCompatActivity {
+public class EventoActivity extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_evento);
 
         ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
         viewPager.setAdapter(new PagerAdapter());
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.navigation_view);
+        navigationView.getMenu().getItem(0).setChecked(true);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.drawer_colaborador) {
+                    drawerLayout.closeDrawers();
+                    startActivity(ColaboradorActivity.newIntent(EventoActivity.this));
+                }
+                return true;
+            }
+        });
+        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_dehaze_white_24dp);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, EventoActivity.class);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     private class PagerAdapter extends FragmentPagerAdapter {
@@ -42,14 +79,12 @@ public class HomeActivity extends AppCompatActivity {
                     return new ProximosEventosFragment();
                 case 1:
                     return new CategoriaEventoFragment();
-                case 2:
-                    return new CategoriaColaboradorFragment();
             }
         }
 
         @Override
         public int getCount() {
-            return 3;
+            return 2;
         }
 
         @Override
@@ -60,8 +95,6 @@ public class HomeActivity extends AppCompatActivity {
                     return getString(R.string.fragment_proximoseventos_title);
                 case 1:
                     return getString(R.string.fragment_categoriaeventos_title);
-                case 2:
-                    return getString(R.string.fragment_categoriacolaboradores_title);
             }
         }
     }
