6910a2c81a04cdf958fc946df7bf1708c6fbf83d
Tássio Auad <auadtassio@gmail.com>
2015-07-11 18:57:50 -0300

    EventoDAO método de listar todos

--- app/src/main/java/br/com/gdgtresrios/centrosulnegocios/model/dao/EventoDao.java
+++ app/src/main/java/br/com/gdgtresrios/centrosulnegocios/model/dao/EventoDao.java
@@ -75,9 +75,35 @@ public class EventoDao {
     public List<Evento> listByCategoria(CategoriaEvento categoriaEvento) {
         List<Evento> eventoList = new ArrayList<>();
         Cursor cursor = database.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_NOME, COLUMN_DESCRICAO,
-                COLUMN_DESCRICAO_DETALHADA, COLUMN_DATA_HORA, COLUMN_DURACAO, COLUMN_LOCAL,
-                COLUMN_FK_CATEGORIA_EVENTO, COLUMN_FK_COLABORADOR}, COLUMN_FK_CATEGORIA_EVENTO + " = ? ",
+                        COLUMN_DESCRICAO_DETALHADA, COLUMN_DATA_HORA, COLUMN_DURACAO, COLUMN_LOCAL,
+                        COLUMN_FK_CATEGORIA_EVENTO, COLUMN_FK_COLABORADOR}, COLUMN_FK_CATEGORIA_EVENTO + " = ? ",
                 new String[]{String.valueOf(categoriaEvento.getId())}, null, null, null);
+
+        while(cursor.moveToNext()) {
+            Evento evento = new Evento();
+            evento.setId(cursor.getLong(0));
+            evento.setNome(cursor.getString(1));
+            evento.setDescricao(cursor.getString(2));
+            evento.setDescricaoDetalhada(cursor.getString(3));
+            evento.setDataHora(new Date(cursor.getLong(4)));
+            evento.setDuracao(new Date(cursor.getLong(5)));
+            evento.setLocal(cursor.getString(6));
+            evento.setCategoriaEvento(new CategoriaEventoDao(database).findById(cursor.getLong(7)));
+            evento.setColaborador(new ColaboradorDao(database).findById(cursor.getLong(8)));
+
+            eventoList.add(evento);
+        }
+
+        cursor.close();
+
+        return eventoList;
+    }
+
+    public List<Evento> listAll() {
+        List<Evento> eventoList = new ArrayList<>();
+        Cursor cursor = database.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_NOME, COLUMN_DESCRICAO,
+                        COLUMN_DESCRICAO_DETALHADA, COLUMN_DATA_HORA, COLUMN_DURACAO, COLUMN_LOCAL,
+                        COLUMN_FK_CATEGORIA_EVENTO, COLUMN_FK_COLABORADOR}, null, null, null, null, null);
 
         while(cursor.moveToNext()) {
             Evento evento = new Evento();
