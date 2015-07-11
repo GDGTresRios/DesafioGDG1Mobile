7ad2670e992ee8ac619de5827013cacc29b45b25
Tássio Auad <auadtassio@gmail.com>
2015-07-11 18:59:47 -0300

    Fragmento de Palestrantes

--- app/src/main/java/br/com/gdgtresrios/centrosulnegocios/model/dao/ColaboradorDao.java
+++ app/src/main/java/br/com/gdgtresrios/centrosulnegocios/model/dao/ColaboradorDao.java
@@ -175,6 +175,38 @@ public class ColaboradorDao {
 
         return colaboradorList;
     }
+
+    public List<Colaborador> listAllPalestrante() {
+        List<Colaborador> colaboradorList = new ArrayList<>();
+        Cursor cursor = database.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_NOME, COLUMN_DESCRICAO, COLUMN_LOGO,
+                        COLUMN_DESCRICAO_DETALHADA, COLUMN_ENDERECO_VIRTUAL, COLUMN_EMAIL, COLUMN_TELEFONE, COLUMN_ENDERECO,
+                        COLUMN_PATROCINADOR, COLUMN_PALESTRANTE, COLUMN_EXPOSITOR, COLUMN_FK_CATEGORIA_COLABORADOR},
+                COLUMN_PALESTRANTE + " = ? ", new String[]{String.valueOf(1)}, null, null, null);
+
+        while (cursor.moveToNext()) {
+            Colaborador colaborador = new Colaborador();
+            colaborador.setId(cursor.getLong(0));
+            colaborador.setNome(cursor.getString(1));
+            colaborador.setDescricao(cursor.getString(2));
+            colaborador.setLogo(cursor.getString(3));
+            colaborador.setDescricaoDetalhada(cursor.getString(4));
+            colaborador.setEnderecoVirtual(cursor.getString(5));
+            colaborador.setEmail(cursor.getString(6));
+            colaborador.setTelefone(cursor.getString(7));
+            colaborador.setEndereco(cursor.getString(8));
+            colaborador.setPatrocinador(cursor.getInt(9) > 0);
+            colaborador.setPalestrante(cursor.getInt(10) > 0);
+            colaborador.setExpositor(cursor.getInt(11) > 0);
+            colaborador.setCategoria(new CategoriaColaboradorDao(database).findById(cursor.getLong(12)));
+
+            colaboradorList.add(colaborador);
+        }
+
+        cursor.close();
+
+        return colaboradorList;
+    }
+
     public Colaborador findById(Long id) {
 
         Cursor cursor = database.query(TABLE_NAME, new String[]{COLUMN_ID, COLUMN_NOME, COLUMN_DESCRICAO, COLUMN_LOGO,
---
+++ app/src/main/java/br/com/gdgtresrios/centrosultnegocios/controller/fragment/PalestrantesFragment.java
@@ -0,0 +1,68 @@
+package br.com.gdgtresrios.centrosultnegocios.controller.fragment;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
+
+import br.com.gdgtresrios.centrosulnegocios.R;
+import br.com.gdgtresrios.centrosulnegocios.model.Colaborador;
+import br.com.gdgtresrios.centrosulnegocios.model.dao.ColaboradorDao;
+import br.com.gdgtresrios.centrosulnegocios.model.dao.DatabaseConnection;
+import br.com.gdgtresrios.centrosultnegocios.controller.listviewadapter.ColaboradorAdapter;
+
+public class PalestrantesFragment extends Fragment {
+
+    private ListView listViewColaborador;
+    private List<Colaborador> colaboradorList;
+
+    private static final String COLABORADORLIST_KEY = "colaboradorlist_key";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view  = inflater.inflate(R.layout.fragment_expositores, container, false);
+
+        if (savedInstanceState != null) {
+            Colaborador[] categoriaColaboradorArray = (Colaborador[]) savedInstanceState.getParcelableArray(COLABORADORLIST_KEY);
+            if(categoriaColaboradorArray != null) {
+                colaboradorList = Arrays.asList(categoriaColaboradorArray);
+            } else {
+                colaboradorList = getAllCategoriaColaboradorFromDatabase();
+            }
+        } else {
+            colaboradorList = getAllCategoriaColaboradorFromDatabase();
+        }
+
+        listViewColaborador = (ListView) view.findViewById(R.id.listview_colaborador);
+        listViewColaborador.setAdapter(new ColaboradorAdapter(colaboradorList, getActivity()));
+
+        return view;
+    }
+
+    private List<Colaborador> getAllCategoriaColaboradorFromDatabase() {
+        SQLiteDatabase databaseConnection = new DatabaseConnection(getActivity()).getWritableDatabase();
+        ColaboradorDao colaboradorDao = new ColaboradorDao(databaseConnection);
+        List<Colaborador> categoriaColaboradorList = colaboradorDao.listAllPalestrante();
+        databaseConnection.close();
+
+        return categoriaColaboradorList;
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        if(colaboradorList != null) {
+            outState.putParcelableArray(COLABORADORLIST_KEY, colaboradorList.toArray(new Colaborador[colaboradorList.size()]));
+        }
+        super.onSaveInstanceState(outState);
+    }
+
+}
+
