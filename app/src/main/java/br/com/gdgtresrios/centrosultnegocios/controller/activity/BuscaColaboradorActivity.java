ddb241f2e06949aff80146b3c41a0857c31c2cbe
Tássio Auad <auadtassio@gmail.com>
2015-07-12 23:07:41 -0300

    Criando busca em todos os colaboradores. O icone para essa ação foi colocada na activity de colaboradores.

--- app/src/main/java/br/com/gdgtresrios/centrosultnegocios/controller/activity/BuscaColaboradorActivity.java
+++ app/src/main/java/br/com/gdgtresrios/centrosultnegocios/controller/activity/BuscaColaboradorActivity.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.NavUtils;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -24,12 +25,10 @@ import br.com.gdgtresrios.centrosulnegocios.model.dao.ColaboradorDao;
 import br.com.gdgtresrios.centrosulnegocios.model.dao.DatabaseConnection;
 import br.com.gdgtresrios.centrosultnegocios.controller.listviewadapter.ColaboradorAdapter;
 
-/**
- * Created by Wanderlei on 12/07/2015.
- */
 public class BuscaColaboradorActivity extends AppCompatActivity {
 
     private static final String INTENT_KEY_CATEGORIACOLABORADOR = "intent_key_categoriaorganizacao";
+    private static final String INTENT_KEY_NOME = "intent_key_categoriaorganizacao";
     private final String BUNDLE_KEY_COLABORADOR = "bundle_key_organizacao";
 
     private ListView listViewColaborador;
@@ -45,6 +44,7 @@ public class BuscaColaboradorActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         categoriaColaborador = getIntent().getParcelableExtra(INTENT_KEY_CATEGORIACOLABORADOR);
+        String nome = getIntent().getStringExtra(INTENT_KEY_NOME);
         if (categoriaColaborador == null){
             getSupportActionBar().setSubtitle(getString(R.string.buscaorganizacaoactivity_substitle_todos));
         } else {
@@ -53,13 +53,13 @@ public class BuscaColaboradorActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         if (savedInstanceState == null){
-            colaboradorList = categoriaColaborador == null ? listColaboradorFromDatabase() : listColaboradorFromDatabase(categoriaColaborador);
+            colaboradorList = listColaboradorFromDatabase(categoriaColaborador, nome);
         } else{
             Colaborador[] categoriaColaboradorArray = (Colaborador[]) savedInstanceState.getParcelableArray(BUNDLE_KEY_COLABORADOR);
             if (categoriaColaboradorArray != null) {
                 colaboradorList = Arrays.asList(categoriaColaboradorArray);
             } else {
-                colaboradorList = categoriaColaborador == null ? listColaboradorFromDatabase() : listColaboradorFromDatabase(categoriaColaborador);
+                colaboradorList = listColaboradorFromDatabase(categoriaColaborador, nome);
             }
         }
 
@@ -67,37 +67,32 @@ public class BuscaColaboradorActivity extends AppCompatActivity {
         listViewColaborador.setAdapter(new ColaboradorAdapter(colaboradorList, this));
     }
 
-    private List<Colaborador> listColaboradorFromDatabase(CategoriaColaborador categoriaColaborador) {
+    private List<Colaborador> listColaboradorFromDatabase(@Nullable CategoriaColaborador categoriaColaborador, @Nullable String nome) {
         SQLiteDatabase databaseConnection = new DatabaseConnection(this).getWritableDatabase();
         ColaboradorDao colaboradorDao = new ColaboradorDao(databaseConnection);
         List<Colaborador> colaboradorList;
-        colaboradorList = colaboradorDao.listByCategoria(categoriaColaborador);
+        if(categoriaColaborador == null) {
+            if(nome == null) {
+                colaboradorList = colaboradorDao.listAll();
+            } else {
+                colaboradorList = colaboradorDao.listByNome(nome);
+            }
+        } else {
+            if(nome == null) {
+                colaboradorList = colaboradorDao.listByCategoria(categoriaColaborador);
+            } else {
+                colaboradorList = colaboradorDao.listByNomeCategoria(categoriaColaborador, nome);
+            }
+        }
         databaseConnection.close();
 
         return colaboradorList;
     }
 
-    private List<Colaborador> listColaboradorFromDatabase(String nome) {
-        SQLiteDatabase databaseConnection = new DatabaseConnection(this).getWritableDatabase();
-        ColaboradorDao colaboradorDao = new ColaboradorDao(databaseConnection);
-        List<Colaborador> colaboradorList = colaboradorDao.listByNome(nome);
-        databaseConnection.close();
-
-        return colaboradorList;
-    }
-
-    private List<Colaborador> listColaboradorFromDatabase() {
-        SQLiteDatabase databaseConnection = new DatabaseConnection(this).getWritableDatabase();
-        ColaboradorDao colaboradorDao = new ColaboradorDao(databaseConnection);
-        List<Colaborador> colaboradorList = colaboradorDao.listAll();
-        databaseConnection.close();
-
-        return colaboradorList;
-    }
-
-    public static Intent newIntent(Context context, CategoriaColaborador categoriaColaborador) {
+    public static Intent newIntent(Context context, @Nullable CategoriaColaborador categoriaColaborador, @Nullable String nome) {
         Intent intent = new Intent(context, BuscaColaboradorActivity.class);
         intent.putExtra(INTENT_KEY_CATEGORIACOLABORADOR, categoriaColaborador);
+        intent.putExtra(INTENT_KEY_NOME, nome);
 
         return intent;
     }
@@ -121,7 +116,7 @@ public class BuscaColaboradorActivity extends AppCompatActivity {
         searchViewAction.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
-                List<Colaborador> colaboradorList = listColaboradorFromDatabase(query);
+                List<Colaborador> colaboradorList = listColaboradorFromDatabase(categoriaColaborador, query);
                 listViewColaborador.setAdapter(new ColaboradorAdapter(colaboradorList, BuscaColaboradorActivity.this));
                 return false;
             }
@@ -129,7 +124,7 @@ public class BuscaColaboradorActivity extends AppCompatActivity {
             @Override
             public boolean onQueryTextChange(String newText) {
                 if (newText.equals("")) {
-                    List<Colaborador> colaboradorList = listColaboradorFromDatabase();
+                    List<Colaborador> colaboradorList = listColaboradorFromDatabase(categoriaColaborador, null);
                     listViewColaborador.setAdapter(new ColaboradorAdapter(colaboradorList, BuscaColaboradorActivity.this));
                 }
                 return false;
